4682cc5d3d16a46696319fed93f85a799549043c
CREDETECH03\CRDE-TECH <gurkan.erginer@crede-tech.com>
2024-12-10 15:43:58 +0300

    navbar düzenlendi

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white py-4 shadow-md">
-      <div className="container mx-auto px-4 flex items-center justify-between">
-        <div className="flex space-x-6">
+    <nav className="bg-blue-600 text-white shadow-md">
+      <div className="container mx-auto px-4 py-4 flex flex-col items-center">
+        <Link
+          href="/"
+          className=" text-2xl font-bold cursor-pointer  text-center mb-4 "
+        >
+          Log Management and File Storage
+        </Link>
+        <div className="flex justify-start w-full space-x-6">
           <Link href="/" className="text-lg hover:underline">
             Home
           </Link>
@@ -24,12 +30,6 @@ export default function Navbar() {
             Download
           </Link>
         </div>
-
-        <Link href="/" className="flex-1 text-center mx-auto">
-          <h1 className="text-2xl font-bold cursor-pointer hover:underline">
-            Log Management and File Storage
-          </h1>
-        </Link>
       </div>
     </nav>
   );
